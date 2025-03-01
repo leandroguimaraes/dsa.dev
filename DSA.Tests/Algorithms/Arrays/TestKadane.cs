c3e3f3eef64e2286358ba2260efb6ac5f2587dd9
Leandro Martins Guimarães <leandropl@gmail.com>
2025-03-01 12:16:36 +0000

    Kadane

---
+++ DSA.Tests/Algorithms/Arrays/TestKadane.cs
@@ -0,0 +1,16 @@
+using DSA.Algorithms.Arrays;
+
+namespace DSA.Tests.Algorithms.Arrays
+{
+    public class TestKadane
+    {
+        [Fact]
+        public void PositiveNumbers() => Assert.Equal(15, Kadane.Run([1, 2, 3, 4, 5]));
+
+        [Fact]
+        public void NegativeNumbers() => Assert.Equal(-1, Kadane.Run([-1, -2, -3, -4, -5]));
+
+        [Fact]
+        public void PositiveNegativeNumbers() => Assert.Equal(6, Kadane.Run([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+    }
+}
---
+++ DSA/Algorithms/Arrays/Kadane.cs
@@ -0,0 +1,21 @@
+namespace DSA.Algorithms.Arrays
+{
+    public class Kadane
+    {
+        public static int Run(int[] arr)
+        {
+            var length = arr.Length;
+
+            var maxOnIndex = arr[0];
+            var result = arr[0];
+
+            for (int i = 1; i < length; i++)
+            {
+                maxOnIndex = Math.Max(arr[i], maxOnIndex + arr[i]);
+                result = Math.Max(result, maxOnIndex);
+            }
+
+            return result;
+        }
+    }
+}
