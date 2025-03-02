867c85c510fa5e11ca054624f44e8a1c9490499a
Leandro Martins Guimarães <leandropl@gmail.com>
2025-03-02 15:10:28 +0000

    Identation fix

--- DSA/Algorithms/PrefixSum/MinSubArrayLen.cs
+++ DSA/Algorithms/PrefixSum/MinSubArrayLen.cs
@@ -1,34 +1,34 @@
 namespace DSA.Algorithms.Arrays
 {
-  public class MinSubArrayLen
-  {
-    // https://leetcode.com/problems/minimum-size-subarray-sum
-    public static int Run(int target, int[] nums)
+    public class MinSubArrayLen
     {
-      var length = nums.Length;
-
-      var currSum = 0;
-      var currLen = 0;
-      var result = 0;
-
-      for (int i = 0; i < length; i++)
-      {
-        currLen++;
-
-        currSum += nums[i];
-        if (currSum >= target)
+        // https://leetcode.com/problems/minimum-size-subarray-sum
+        public static int Run(int target, int[] nums)
         {
-          if (result == 0 || result > currLen)
-          {
-            result = currLen;
-          }
+            var length = nums.Length;
 
-          currLen = 0;
-          currSum = 0;
+            var currSum = 0;
+            var currLen = 0;
+            var result = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                currLen++;
+
+                currSum += nums[i];
+                if (currSum >= target)
+                {
+                    if (result == 0 || result > currLen)
+                    {
+                        result = currLen;
+                    }
+
+                    currLen = 0;
+                    currSum = 0;
+                }
+            }
+
+            return result;
         }
-      }
-
-      return result;
     }
-  }
 }
